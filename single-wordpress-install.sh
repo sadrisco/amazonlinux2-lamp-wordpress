af7dc7ef9993b7b66b3e0c1f121ff7c2cb512d5a
Rickson Figueira <risco@sadrisco.tk>
2020-07-12 21:11:41 -0300

    Update single-wordpress-install.sh

--- single-wordpress-install.sh
+++ single-wordpress-install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Originally written by Artnic, updated by sadrisco
-# You can put this script on an Amazon Linux AMI 2 to setup a Website quickly.
+# You can put this script on an Amazon Linux AMI 2 to setup a Wordpress website quickly.
 # Requires a LAMP installation
 
 
-_SITE_URL_='site.example.com' # don't use protocol
+_SITE_URL_='site.example.com' # don't use protocols
 _DB_ROOT_PASS_='ReallyStrongDatabaseRootPassword'
 _DB_USER_='MySQLUserAllowedToAccessFromOutside'
 _DB_USERPASS_='CreatedMySQLUsersPassword'
@@ -13,11 +13,11 @@ _WP_SITE_TITLE_='Wordpress Site Title'
 _WP_ADMIN_PASS_='WordpressAdminPassword'
 _WP_ADMIN_EMAIL_='WordpressAdminEmail' # once completed, server sends 'installation completed' email to this one
 
-# Do this to know which sites are installed in this machine quickly
+# do this to know which sites are installed in this machine quickly
 touch /home/ec2-user/${_SITE_URL_}
 
 sudo yum update -y
-# configuring httpd.conf and adding a site (VirtualHost) to it
+# configuring httpd.conf and virtualhost
 sudo mkdir -p /var/www/html/${_SITE_URL_}
 sudo chmod +x /var/www/html/${_SITE_URL_} -Rf
 
@@ -42,19 +42,8 @@ mysqladmin -u root password ${_DB_ROOT_PASS_}
 mysql -uroot -p${_DB_ROOT_PASS_} -e "GRANT ALL on *.* to '${_DB_USER_}'@'localhost' identified by '${_DB_USERPASS_}';grant all on *.* to '${_DB_USER_}'@'%' identified by '${_DB_USERPASS_}'; FLUSH PRIVILEGES;"
 mysqladmin -uroot -p${_DB_ROOT_PASS_} create ${_DB_NAME_}
 
-# BONUS: installing Wordpress via wpcli with given URL and database
-# You should remove lines below if you don't want to fresh install wordpress
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
-/usr/local/bin/wp --info
-sudo mkdir /opt/wpcli/
-cd  /opt/wpcli/
-sudo wget https://raw.githubusercontent.com/wp-cli/wp-cli/master/utils/wp-completion.bash
-sudo echo 'source /opt/wpcli/wp-completion.bash' >> /root/.bash_profile
-sudo echo 'source /opt/wpcli/wp-completion.bash' >> /home/ec2-user/.bash_profile
+# installing wordpress
 cd /var/www/html/${_SITE_URL_}
-
 sudo /usr/local/bin/wp core download --allow-root
 sudo /usr/local/bin/wp core config --dbhost=localhost --dbname=${_DB_NAME_} --dbuser=${_DB_USER_} --dbpass=${_DB_USERPASS_} --allow-root
 sudo /usr/local/bin/wp core install --url=${_SITE_URL_} --title="${_WP_SITE_TITLE_}" --admin_name=admin --admin_password=${_WP_ADMIN_PASS_} --admin_email=${_WP_ADMIN_EMAIL_} --allow-root
